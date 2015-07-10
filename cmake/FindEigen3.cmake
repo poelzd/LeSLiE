f0f8027d8711273b5b6e3be56951765ccd9f7d82
Dominik Poelz <dominik.poelz@student.tugraz.at>
2015-07-10 09:25:07 +0200

    set up CMakeLists.txt (for lsqMain.C); included file in folder cmake to find Eigen library; no actual code yet

---
+++ cmake/FindEigen3.cmake
@@ -0,0 +1,25 @@
+# - Find Eigen
+# Find the native Eigen includes and library
+#
+# EIGEN_INCLUDES - where to find signature_of_eigen3_matrix_library
+# EIGEN_LIBRARIES - List of libraries when using EIGEN.
+# EIGEN_FOUND - True if EIGEN found.
+ 
+if (EIGEN_INCLUDES)
+  # Already in cache, be silent
+  set (EIGEN_FIND_QUIETLY TRUE)
+endif (EIGEN_INCLUDES)
+ 
+find_path(EIGEN_INCLUDES NAMES signature_of_eigen3_matrix_library
+  PATHS
+  /usr/include
+  /usr/local/include
+  ${HYENA_CORE_BASE_PATH}/contrib/eigen
+  PATH_SUFFIXES eigen3 eigen
+  )
+ 
+# handle the QUIETLY and REQUIRED arguments and set EIGEN_FOUND to TRUE if
+# all listed variables are TRUE
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (EIGEN DEFAULT_MSG EIGEN_INCLUDES)
+mark_as_advanced (EIGEN_INCLUDES)
---
+++ src/lsqMain.C
@@ -0,0 +1,10 @@
+// my first program in C++
+#include <iostream>
+
+#include <Eigen/Dense>
+
+int main()
+{
+  Eigen::MatrixXd X(3,3);
+  std::cout << "Hello World!";
+}
